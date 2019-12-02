e6587ecb52fd8872874663bcd729b6405036e7b7
José Cabrera <40355414+Jose-Cabrera-FullStack@users.noreply.github.com>
2019-12-01 21:11:31 -0300

    Preparando para backupo

---
+++ src/containers/Boosting.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+import Footer from '../components/Footer';
+
+import '../assets/styles/App.scss';
+
+const Boosting = () => {
+  return(
+    <div className="App">
+      <Header/>
+      
+      <Footer/>
+    </div>
+
+  );
+}
+
+export default Boosting;
